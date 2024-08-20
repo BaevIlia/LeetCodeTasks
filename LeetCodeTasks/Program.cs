2292d9ac88f9d082a8a47e18eb08de99831a896b
Ilia Baev <baevilia.201@yandex.ru>
2024-08-20 16:45:31 +0300

    Добавьте файлы проекта.

---
+++ LeetCodeTasks/Program.cs
@@ -0,0 +1,12 @@
+﻿using LeetCodeTasks.Tasks;
+namespace LeetCodeTasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Task35 task35 = new();
+            Console.WriteLine(task35.SearchInsert([1, 3, 5, 6], 7));
+        }
+    }
+}
---
+++ LeetCodeTasks/Tasks/Task35.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+
+namespace LeetCodeTasks.Tasks
+{
+    public class Task35
+    {
+        
+        public int SearchInsert(int[] nums, int target) 
+        {
+            const int errorFlag = -1;
+            int length = nums.Length;
+            int half = length / 2;
+            int result = errorFlag;
+            if (target > nums[half])
+            {
+                for (int i = Array.IndexOf(nums, nums[half]); i < length; i++)
+                {
+                    if (target == nums[i]) 
+                    {
+                        result = Array.IndexOf(nums, nums[i]);
+                        return result;
+                    }
+                       
+                   
+                }
+            }
+            else if (target < nums[half])
+            {
+                for (int i = 0; i <= half; i++)
+                {
+                    if (target == nums[i])
+                    {
+                        result = Array.IndexOf(nums, nums[i]);
+                        return result;
+                    }
+                    
+                }
+            }
+            else if (target == nums[half])
+            {
+                result = Array.IndexOf(nums, nums[half]);
+                return result;
+            }
+
+            if (result == errorFlag) 
+            {
+                int count = half;
+                if (target > nums[length - 1]) 
+                {
+                    result = Array.IndexOf(nums, nums[length - 1]);
+                    return result;
+                }
+                if (target < nums[0])
+                {
+                    result = 0;
+                    return result;
+                }
+                if (target > nums[half]) 
+                {
+                   
+                    while (target > nums[count]) 
+                    {
+                        count++;
+                    }
+                    result = Array.IndexOf(nums, nums[count]);
+                    return result;
+                }
+                else if (target < nums[half])
+                {
+                   
+                    while (target < nums[count])
+                    {
+                        count--;
+                    }
+                    result = Array.IndexOf(nums, nums[count])+1;
+                    return result;
+                }
+            }
+
+            return result;
+            
+        }
+    }
+}
