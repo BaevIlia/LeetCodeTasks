55fec8923917e0106fa0e9bce1257947645b7c3f
Ilia Baev <baevilia.201@yandex.ru>
2024-08-21 20:16:13 +0300

    Два решения к задаче 27

--- LeetCodeTasks/Program.cs
+++ LeetCodeTasks/Program.cs
@@ -7,9 +7,11 @@ namespace LeetCodeTasks
         {
            // Task35 task35 = new();
            // Console.WriteLine(task35.SearchInsert([1, 3, 5, 6], 7));
-           Task20 task20 = new();
-           Console.WriteLine(task20.IsValid("["));
-           
+           //Task20 task20 = new();
+          // Console.WriteLine(task20.IsValid("["));
+           Task27 task27 = new Task27();
+            Console.WriteLine(task27.RemoveElement([3, 2, 2, 3], 3));
+
         }
     }
 }
---
+++ LeetCodeTasks/Tasks/Task27.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace LeetCodeTasks.Tasks
+{
+    public class Task27
+    {
+        public int RemoveElement(int[] nums, int val) 
+        {
+
+            //Не проходит тесты
+            /*int[] newNums = nums.Where(i=>i!=val).ToArray();
+            return newNums.Length;*/
+
+            int k = 0;
+
+            for (int i = 0; i < nums.Length; i++) 
+            {
+                if (nums[i] != val) 
+                {
+                    nums[k] = nums[i];
+                    k++;
+                }
+            }
+
+            return k;
+        }
+
+    }
+}
