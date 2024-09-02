e065377efc03f066323bfd1ad911dc103dea3f5e
Ilia Baev <baevilia.201@yandex.ru>
2024-09-02 17:03:38 +0300

    Решение 17й задачи

--- LeetCodeTasks/Program.cs
+++ LeetCodeTasks/Program.cs
@@ -9,8 +9,11 @@ namespace LeetCodeTasks
            // Console.WriteLine(task35.SearchInsert([1, 3, 5, 6], 7));
            //Task20 task20 = new();
           // Console.WriteLine(task20.IsValid("["));
-           Task27 task27 = new Task27();
-            Console.WriteLine(task27.RemoveElement([3, 2, 2, 3], 3));
+          /* Task27 task27 = new Task27();
+            Console.WriteLine(task27.RemoveElement([3, 2, 2, 3], 3));*/
+
+            Task17 task17 = new Task17();
+            Console.WriteLine(task17.LetterCombinations("23"));
 
         }
     }
---
+++ LeetCodeTasks/Tasks/Task17.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTasks.Tasks
+{
+    public class Task17
+    {
+        public IList<string> LetterCombinations(string digits)
+        {
+            List<string> resultList = new();
+            if (string.IsNullOrWhiteSpace(digits))
+                return resultList;
+
+
+            Dictionary<char, string> numbersLetters = new()
+            {
+                {'2', "abc" },
+                {'3', "def" },
+                {'4', "ghi" },
+                {'5', "jkl" },
+                {'6', "mno" },
+                {'7', "pqrs"},
+                {'8', "tuv" },
+                {'9', "wxyz" }
+
+
+            };
+
+            resultList.Add("");
+            for (int i = 0; i < digits.Length; i++)
+            {
+                resultList = resultList.SelectMany(x => numbersLetters[digits[i]].Select(y => x + y.ToString())).ToList();
+            }
+
+
+            
+            return resultList;
+        }
+    }
+}
