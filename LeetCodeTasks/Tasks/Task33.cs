6671de6e0cfb8421274ec83c90525c8d47a2b082
Ilia Baev <baevilia.201@yandex.ru>
2024-09-06 17:30:27 +0300

    Решение 33й задачи

---
+++ LeetCodeTasks/Tasks/Task33.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTasks.Tasks
+{
+    public class Task33
+    {
+        public int Search(int[] nums, int target)
+        {
+            int leftPointer = 0;
+            int rightPointer = nums.Length - 1;
+            int middle = 0;
+            while (leftPointer <= rightPointer) 
+            {
+                middle = (leftPointer + rightPointer) / 2;
+                if (target == nums[middle]) 
+                {
+                    return middle;
+                }
+                if (nums[middle] >= nums[leftPointer])
+                {
+                    if (nums[leftPointer] <= target && target <= nums[middle])
+                    {
+                        rightPointer = middle - 1;
+                    }
+                    else
+                    {
+                        leftPointer = middle + 1;
+                    }
+                }
+                else 
+                {
+                    if (nums[middle] <= target && target <= nums[rightPointer])
+                    {
+                        leftPointer = middle + 1;
+                    }
+                    else 
+                    {
+                        rightPointer = middle - 1;
+                    }
+                }
+            }
+
+            return -1;
+        }
+    }
+}
