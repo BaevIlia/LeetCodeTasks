c828931ea1d853ab6f2412d923e22e4a05755f86
Ilia Baev <baevilia.201@yandex.ru>
2024-09-06 11:59:46 +0300

    Добавлено решение 50й задачи

--- LeetCodeTasks/Program.cs
+++ LeetCodeTasks/Program.cs
@@ -5,16 +5,21 @@ namespace LeetCodeTasks
     {
         static void Main(string[] args)
         {
-           // Task35 task35 = new();
-           // Console.WriteLine(task35.SearchInsert([1, 3, 5, 6], 7));
-           //Task20 task20 = new();
-          // Console.WriteLine(task20.IsValid("["));
-          /* Task27 task27 = new Task27();
-            Console.WriteLine(task27.RemoveElement([3, 2, 2, 3], 3));*/
+            // Task35 task35 = new();
+            // Console.WriteLine(task35.SearchInsert([1, 3, 5, 6], 7));
+            //Task20 task20 = new();
+            // Console.WriteLine(task20.IsValid("["));
+            /* Task27 task27 = new Task27();
+              Console.WriteLine(task27.RemoveElement([3, 2, 2, 3], 3));*/
 
-            Task17 task17 = new Task17();
-            Console.WriteLine(task17.LetterCombinations("23"));
+            /*Task17 task17 = new Task17();
+            Console.WriteLine(task17.LetterCombinations("23"));*/
 
+            Task50 task50 = new Task50();
+            Console.WriteLine(task50.MyPow(2.00000, -2147483648));
+
+
+          
         }
     }
 }
